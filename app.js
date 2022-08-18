86160ce010e4dadf6d9612633061cc0d91f09d01
ligia05 <92107626+ligia05@users.noreply.github.com>
2022-08-18 11:34:17 -0300

    quitou

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./Router/index');
+var usersRouter = require('./Router/user');
 
 var app = express();
 
